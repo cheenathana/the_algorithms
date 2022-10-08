007e1373ceca164109e080a1c5cf711457e56fcd
Cheena Thana <111827546+cheenathana@users.noreply.github.com>
2022-10-08 13:47:26 +0530

    added sorting function

--- two_number_sum/two_number_sum.c
+++ two_number_sum/two_number_sum.c
@@ -1,4 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
+
+int compare_val(const void* x, const void* y) {
+   // Makeing the pointer to int pointer
+   int element1 = *((int*)x);
+   int element2 = *((int*)y);
+
+   if (element1 > element2)
+      return 1;
+   
+   if (element1 < element2)
+      return -1;
+
+   return 0;
+}
+
+
 
 void two_number_sum(int arr[], int size, int targetsum, int out[]) {
    int lptr = 0;
@@ -24,8 +41,11 @@ void two_number_sum(int arr[], int size, int targetsum, int out[]) {
 /* EXECUTION BEGINS HERE */
 int main() {
    int out[2];
-   int arr[] = {3, 5, -1, 8, 11, 1, -1};
+   int arr[] = {3, 5, 44, 8, 11, 1, -1};
    int targetsum = 10;
+
+   // Sort the array
+   qsort(arr, sizeof(arr)/sizeof(arr[0]), sizeof(arr[0]), compare_val);
 
    two_number_sum(arr, sizeof(arr)/sizeof(arr[0]), targetsum, out);
 
