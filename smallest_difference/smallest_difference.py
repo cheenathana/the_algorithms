76a6b31814e14f0ed4764e23f0134879e375bbb8
Cheena Thana <111827546+cheenathana@users.noreply.github.com>
2022-10-09 23:07:19 +0530

    smallest difference

---
+++ smallest_difference/smallest_difference.py
@@ -0,0 +1,27 @@
+"""
+Input:
+    arr1
+    arr2
+
+Output:
+    Number pair from arr1 and arr2 whoses difference is smallest
+
+Algorithm:
+    1. 
+    2. 
+
+Time complexity  :
+Space complexity :
+"""
+
+def smallest_difference():
+    pass
+
+
+
+# Execution begins here
+if __name__ = "__main__":
+    arr1 = [];
+    arr2 = [];
+
+
