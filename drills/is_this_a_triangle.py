89cb3087c0c973b5217cfb6b40c77319b2d508f6
Cheena Thana <111827546+cheenathana@users.noreply.github.com>
2022-10-22 16:33:37 +0530

    drill 004

---
+++ drills/is_this_a_triangle.py
@@ -0,0 +1,14 @@
+"""
+Implement a function that accepts 3 integer values a, b, c. The function should
+return true if a triangle can be built with the sides of given length and false
+in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+"""
+def is_triangle(a, b, c):
+   s = sorted([a, b, c])
+   return s[2] < s[0] + s[1]
+
+# Other solution
+def is_triangle(a, b, c):
+    return (a<b+c) and (b<a+c) and (c<a+b)
