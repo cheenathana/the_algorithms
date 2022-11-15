7a878cb08664aac3fb9c0abb1bc61875af392add
Cheena Thana <111827546+cheenathana@users.noreply.github.com>
2022-11-15 19:18:26 +0530

    removel of broken code

--- pyslugs/deep_flatten/deep_flatten.py
+++
@@ -1,9 +0,0 @@
-def deep_flatten(data):
-   val = []
-   for i in data:
-      for j in i:
-         if isinstance(j, (list, tuple):
-            val.extend(j)
-         else:
-            val.append(j)
-         
